6dc31359bf6271389c10744369429f3ea1864f2d
RickyOW <44971832+RickyOW@users.noreply.github.com>
2018-12-11 18:42:09 +0700

    admin

--- Antrian/app/src/main/java/com/ricky/com/gosalon/Admin/TransaksiFragment.java
+++ Antrian/app/src/main/java/com/ricky/com/gosalon/Admin/TransaksiFragment.java
@@ -25,7 +25,7 @@ public class TransaksiFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_transaksi, container, false);
+        return inflater.inflate(R.layout.fragmenta_admin_transaksi, container, false);
     }
 
 }
