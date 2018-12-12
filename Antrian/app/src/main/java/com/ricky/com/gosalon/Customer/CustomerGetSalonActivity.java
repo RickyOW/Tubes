49db69611670aba09855786364778602c6632905
RickyOW <44971832+RickyOW@users.noreply.github.com>
2018-12-12 23:53:51 +0700

    memperbaiki activity_customer_getsalon

--- Antrian/app/src/main/java/com/ricky/com/gosalon/Customer/CustomerGetSalonActivity.java
+++ Antrian/app/src/main/java/com/ricky/com/gosalon/Customer/CustomerGetSalonActivity.java
@@ -1,15 +1,18 @@
 package com.ricky.com.gosalon.Customer;
 
 import android.content.Intent;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 import com.ricky.com.gosalon.R;
 
 public class CustomerGetSalonActivity extends AppCompatActivity {
-    Button btPesan,btMap;
+    FloatingActionButton btMap;
+    TextView btPesan;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
